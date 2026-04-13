8dd21ad86ebebb23c426d24c955318fa128a9cfe
BabouZ <dimitri.estrade@gmail.com>
2026-04-13 16:42:18 -0400

    Add: find-anagram-mappings - submission-1

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = list()
+
+        vals_to_indexes = dict()
+        for i in range(len(nums2)):
+            vals_to_indexes[nums2[i]] = i
+
+        for i in range(len(nums1)):
+            res.append(vals_to_indexes[nums1[i]])
+
+        return res
