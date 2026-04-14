fe0ce9e4c5bbd3b2b4652e769c27a76c3d4b7e70
BabouZ <dimitri.estrade@gmail.com>
2026-04-14 17:18:47 -0400

    Add: sort-array-by-increasing-frequency - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-1.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freqs = Counter(nums)
+        nums.sort(key=lambda x: (freqs[x], -x))
+        return nums
+                
