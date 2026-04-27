47a8ad00e926f689b42d62ae5472a79a678ac3e9
BabouZ <dimitri.estrade@gmail.com>
2026-04-26 20:40:12 -0400

    Add: strobogrammatic-number - submission-1

---
+++ Data Structures & Algorithms/strobogrammatic-number/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        mapVals = {
+            "0": "0",
+            "1": "1",
+            "6": "9",
+            "8": "8",
+            "9": "6"
+        }
+
+        l, r = 0, len(num) - 1
+        while l <= r:
+            if num[l] not in mapVals:
+                return False
+
+            if mapVals[num[l]] != num[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
