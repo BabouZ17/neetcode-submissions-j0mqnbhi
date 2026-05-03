2149732e15426c318ff20f66883e584de60130a7
BabouZ <dimitri.estrade@gmail.com>
2026-05-02 20:03:24 -0400

    Add: one-edit-distance - submission-0

---
+++ Data Structures & Algorithms/one-edit-distance/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        len_s, len_t = len(s), len(t)
+
+        if len_s > len_t:
+            return self.isOneEditDistance(t, s)
+
+        if len_t - len_s > 1:
+            return False
+
+        for i in range(len_s):
+            if s[i] != t[i]:
+                if len_s == len_t:
+                    return s[i+1:] == t[i+1:]
+                else:
+                    return s[i:] == t[i+1:]
+        return len_s + 1 == len_t
