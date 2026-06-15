30050cfa1edfe134ea40f2b4a5610aca555a0d7c
BabouZ <dimitri.estrade@gmail.com>
2026-06-16 00:36:42 +0200

    Add: remove-duplicates-from-an-unsorted-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-an-unsorted-linked-list/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
+        duplicates = set()
+        seen = set()
+
+        curr = head
+        while curr:
+            if curr.val in seen:
+                duplicates.add(curr.val)
+            else:
+                seen.add(curr.val)
+            curr = curr.next
+
+        dummy = ListNode(-1)
+        dummy.next = head
+        curr = dummy
+        while curr and curr.next:
+            if curr.next.val in duplicates:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return dummy.next
